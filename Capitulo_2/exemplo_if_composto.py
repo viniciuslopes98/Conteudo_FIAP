039d52e4cde7ec2616af6d3045de764fbf8c6522
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-24 13:45:40 -0300

    Att conteúdo

--- Capitulo_2/exemplo_if_composto.py
+++ Capitulo_2/exemplo_if_composto.py
@@ -3,3 +3,5 @@ idade = input("Insira sua idade")
 if int(idade) >= 18:
     print("Sua participação foi autorizada, aluno de RM {}!".format(rm))
     print("Mais informações serão enviadas para seu e-mail cadastrado!")
+else:
+    print("Sua participação não foi autorizada por causa da sua idade")
