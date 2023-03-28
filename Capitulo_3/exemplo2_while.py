dc7b00c9c58b4b3758abfd26c7a8f57e993487a8
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-28 08:19:54 -0300

    Exemplos de while

---
+++ Capitulo_3/exemplo2_while.py
@@ -0,0 +1,6 @@
+numero = 1
+
+while numero % 2 == 1:
+    numero = int(input("Digite um número par!"))
+
+print("Parabéns, você conseguiu!")
---
+++ Capitulo_3/exemplo_while.py
@@ -0,0 +1,13 @@
+resposta = " "
+
+while resposta != "42":
+    resposta = input("Qual a resposta para a vida, o universo e tudo mais ?")
+
+print("Parabéns, você é um verdadeiro geek!")
+
+i = 0
+
+while (i<10):
+    #para cada uma das repetições uma mensagem é exibida
+    print("Mais uma mensagem, com i valendo: {}".format(i))
+    i = i + 1
