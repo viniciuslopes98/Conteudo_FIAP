c21e2f48cda7b33eb1522307f6221a269913c9db
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-28 09:10:44 -0300

    Exemplos de For e tabuada com While

---
+++ Capitulo_3/exemplo_for.py
@@ -0,0 +1,18 @@
+#a variável contadora deverá assumir valores no intervalo entre 0 e 9
+for x in range(10):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(x)
+
+print('-----')
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 14
+for y in range(1,15):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(y)
+
+print('-----')
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 9 com incremento 2
+for x in range(1,10,2):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(x)
---
+++ Capitulo_3/tabuada_while.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um número para ver a tabuada: "))
+i = 1
+
+while i <= 10:
+    print(" {} x {} = {}".format(n, i,n * i))
+    i = i + 1
+
